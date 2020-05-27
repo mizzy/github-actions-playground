6f9aea341b0ab66b9fd1f5209bb1a31439383f54
Gosuke Miyashita <gosukenator@gmail.com>
2020-05-27 21:03:31 +0900

    commit

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -23,9 +23,9 @@ for dir in $dirs; do
     cd $dir
     terraform init
     if [ "$1" = "fmt" ]; then
-        terraform fmt -check -diff | tfnotify --config ../../.tfnotify.yml fmt
+        terraform fmt -check -diff | tfnotify --config ../../.tfnotify.yml fmt -t "## ${dir}"
     elif [ "$1" = "plan" ]; then
-        terraform plan -refresh=false | tfnotify --config ../../.tfnotify.yml plan
+        terraform plan -refresh=false | tfnotify --config ../../.tfnotify.yml plan -t "## ${dir}"
     elif [ "$1" = "apply" ]; then
         terraform apply -auto-approve
     fi
